217a7ebd610c31c1a70b55a0d328d0f6e24c64e1
MOHAMMED ISHRAK FAISAL <170192642+IshrakFaisal@users.noreply.github.com>
2026-05-08 21:23:18 +0600

    Remove irrelevant scaffold files and launchers

--- Open Productivity Hub.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-start "Productivity Hub Dev Server" cmd /k "npm.cmd run dev -- --hostname 127.0.0.1 --port 3000"
-timeout /t 4 /nobreak > nul
-start "" "http://127.0.0.1:3000"
